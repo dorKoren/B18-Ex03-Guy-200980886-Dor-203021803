114e142e0f0df31b8c12657588b851c51012b79b
dor.koren <dor.koren@post.idc.ac.il>
2018-05-14 11:52:14 +0300

    dor - start to working on RefuelFuelBasedVehicle method

--- Ex03.GarageLogic/Garage.cs
+++ Ex03.GarageLogic/Garage.cs
@@ -167,7 +167,9 @@ Vehicle status: {2}", OwnerName, OwnerPhone, VehicleStatus);
 
 
 
-        public void ChangeVehicleStatus(string i_LicenseNumber, VehicleDetails.eVehicleStatus i_DesiredStatus)
+        public void ChangeVehicleStatus(
+            string i_LicenseNumber,
+            VehicleDetails.eVehicleStatus i_DesiredStatus)
         {
             // Exception !!!
 
@@ -182,7 +184,7 @@ Vehicle status: {2}", OwnerName, OwnerPhone, VehicleStatus);
         {
             // Exception !!!
 
-            VehicleList.TryGetValue(i_LicenseNumber, out Vehicle vehicle);  // upcasting!!!
+            VehicleList.TryGetValue(i_LicenseNumber, out Vehicle vehicle);       // upcasting!!!
 
             foreach (Wheel wheel in vehicle.Wheels)
             {
@@ -193,15 +195,24 @@ Vehicle status: {2}", OwnerName, OwnerPhone, VehicleStatus);
         }
 
 
-        public void RefuelFuelBasedVehicle(string i_LicenseNumber, eFuelType i_FuelType, float i_RefuelAmount)
+        public void RefuelFuelBasedVehicle(                                
+            string i_LicenseNumber,
+            eFuelType i_FuelType,
+            float i_RefuelAmount)
         {
             // Exception !!!
             // TODO: complete.
+            if (VehicleList.TryGetValue(i_LicenseNumber, out Vehicle vehicle))   // down casting !!!
+            {
+
+            }
 
 
         }
 
-        public void ChargeElectricBasedVehicle(string i_LicenseNumber, float i_ChargingAmount)
+        public void ChargeElectricBasedVehicle(
+            string i_LicenseNumber,
+            float i_ChargingAmount)
         {
             // Exception ???
             // TODO: complete.
@@ -247,7 +258,8 @@ Vehicle status: {2}", OwnerName, OwnerPhone, VehicleStatus);
             return type;
         }
 
-        private string printLicenseNumbersList(Dictionary<string, VehicleDetails> i_LicenseNumbers)  // How can we make it to be polimorfic?
+        private string printLicenseNumbersList(
+            Dictionary<string, VehicleDetails> i_LicenseNumbers)  // How can we make it to be polimorfic?
         {
             StringBuilder sb = new StringBuilder();
 
